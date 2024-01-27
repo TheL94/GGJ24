8fc5ad876125cf7d7fc38fb8f6fc0ade4fe23fa4
SplitFace <masi.edoardo@libero.it>
2024-01-27 19:27:31 +0100

    Reworked RoombaFov for optimization purposes

--- Assets/Scripts/Roomba/Behaviours/RoombaFOV.cs
+++ Assets/Scripts/Roomba/Behaviours/RoombaFOV.cs
@@ -2,59 +2,65 @@ using UnityEngine;
 
 public class RoombaFOV : MonoBehaviour
 {
-    public LayerMask mask;
+    float RayDegree { get => nRay % 2 != 0 ? (float)(degrees / nRay - 1) : (degrees / 2) / (float)nRay; }
+
     public int nRay;
     public float degrees;
     public float distance;
 
-    public int leftCasted = 0;
-    public int rightCasted = 0;
-
-    void FixedUpdate()
+    private bool ShootStraightRay(out RaycastHit hit, LayerMask mask)
     {
-        leftCasted = 0;
-        rightCasted = 0;
+        hit = new RaycastHit();
 
-        RaycastHit hit;
-        float rayForDegree = 0f;
-        if(nRay % 2 != 0)
-        {
-            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, distance, mask))
-            {
-                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
-                leftCasted++;
-                rightCasted++;
-            }
-            else
-                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * distance, Color.magenta);
-
-            rayForDegree = (float)(degrees / nRay - 1);
-        }
+        if (nRay % 2 != 0)
+            return Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, distance, mask);
         else
-            rayForDegree = (degrees / 2) / (float)nRay;
+            return false;
+    }
 
-        //LEFT
+    public int CountRaysSideways(bool rightSide)
+    {
+        float angle = rightSide ? RayDegree : -RayDegree;
+
+        // ~0 equals "everything" for layermask
+        int hits = ShootStraightRay(out RaycastHit hit, ~0) ? 1 : 0;
+
         for (int i = 1; i < nRay / 2 + 1; i++)
         {
-            if (Physics.Raycast(transform.position, Quaternion.AngleAxis(-rayForDegree * i, Vector3.up) * transform.forward, out hit, distance, mask))
+            if (Physics.Raycast(transform.position, Quaternion.AngleAxis(angle * i, Vector3.up) * transform.forward, out hit, distance, ~0))
             {
-                Debug.DrawRay(transform.position, Quaternion.AngleAxis(-rayForDegree * i, Vector3.up) * transform.forward, Color.yellow);
-                leftCasted++;
+                Debug.DrawRay(transform.position, Quaternion.AngleAxis(angle * i, Vector3.up) * transform.forward, Color.yellow);
+                hits++;
             }
             else
-                Debug.DrawRay(transform.position, Quaternion.AngleAxis(-rayForDegree * i, Vector3.up) * transform.forward * distance, Color.magenta);
+                Debug.DrawRay(transform.position, Quaternion.AngleAxis(angle * i, Vector3.up) * transform.forward * distance, Color.magenta);
         }
 
-        //RIGHT
+        return hits;
+    }
+
+    public bool FindObject(LayerMask mask, out RaycastHit hit)
+    {
+        return FindObjectSideways(true, out hit, mask) || FindObjectSideways(false, out hit, mask);
+    }
+
+    private bool FindObjectSideways(bool rightSide, out RaycastHit hit, LayerMask mask)
+    {
+        float angle = rightSide ? RayDegree : -RayDegree;
+
+        if (ShootStraightRay(out hit, mask))
+            return true;
+
         for (int i = 1; i < nRay / 2 + 1; i++)
         {
-            if (Physics.Raycast(transform.position, Quaternion.AngleAxis(rayForDegree * i, Vector3.up) * transform.forward, out hit, distance, mask))
+            if (Physics.Raycast(transform.position, Quaternion.AngleAxis(angle * i, Vector3.up) * transform.forward, out hit, distance, ~0))
             {
-                Debug.DrawRay(transform.position, Quaternion.AngleAxis(rayForDegree * i, Vector3.up) * transform.forward, Color.yellow);
-                rightCasted++;
+                Debug.DrawRay(transform.position, Quaternion.AngleAxis(angle * i, Vector3.up) * transform.forward, Color.yellow);
+                return true;
             }
             else
-                Debug.DrawRay(transform.position, Quaternion.AngleAxis(rayForDegree * i, Vector3.up) * transform.forward * distance, Color.magenta);
+                Debug.DrawRay(transform.position, Quaternion.AngleAxis(angle * i, Vector3.up) * transform.forward * distance, Color.magenta);
         }
+        return false;
     }
 }
