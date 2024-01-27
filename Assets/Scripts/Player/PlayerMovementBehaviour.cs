40e10d19c6b21624288027ae78d145f01ff21525
Luca Garbelli <garbelli.luca.94@gmail.com>
2024-01-27 14:01:51 +0100

    Start working

--- Assets/Scripts/Player/PlayerMovementBehaviour.cs
+++ Assets/Scripts/Player/PlayerMovementBehaviour.cs
@@ -1,45 +1,30 @@
 ﻿using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.XR;
+using UnityEngine.Serialization;
 
 public class PlayerMovementBehaviour : MonoBehaviour
 {
+    public float Speed { get; private set; } = 0.0f;
+    public bool Grounded => m_Grounded;
+    
     public bool abilityPermitted = true;
-    public Camera MainCamera;
-
-    public Transform CameraPosition;
 
     [Header("Control Settings")]
-    public float mouseSensitivity = 100.0f;
     public float controllerRotationMultiplier = 2.0f;
     public float playerSpeed = 5.0f;
-    public float RunningSpeed = 7.0f;
+    public float runningSpeed = 7.0f;
     public float jumpSpeed = 5.0f;
     public float pushPower = 2.0f;
     public float gravityValue = 2.0f;
+    public float rotateAngleThreshold;
 
-#if UNITY_ANDROID || UNITY_WEBGL
-        public float androidMultiplier = 2;
-
-        [HideInInspector]
-        public Vector2 touchMovement = Vector2.zero;
-        public Vector2 touchRotation = Vector2.zero;
-#endif
-
-    float m_VerticalSpeed = 0.0f;
-
+    float m_VerticalSpeed;
     float m_VerticalAngle, m_HorizontalAngle;
-    public float Speed { get; private set; } = 0.0f;
-
-    public bool Grounded => m_Grounded;
-
     CharacterController m_CharacterController;
-
     bool m_Grounded;
     float m_GroundedTimer;
-    float m_SpeedAtJump = 0.0f;
-    Vector3 moveVelocity = new Vector3();
-
+    float m_SpeedAtJump;
+    Vector3 moveVelocity;
     PlayerInput playerInput;
     InputAction movement;
     InputAction rotation;
@@ -54,17 +39,9 @@ public class PlayerMovementBehaviour : MonoBehaviour
     void Start()
     {
         movement = playerInput.actions.FindAction("Movement");
-        rotation = playerInput.actions.FindAction("Rotation");
         jump = playerInput.actions.FindAction("Jump");
-
         jump.performed += Jump;
-
         m_Grounded = true;
-
-        MainCamera.transform.SetParent(CameraPosition, false);
-        MainCamera.transform.localPosition = Vector3.zero;
-        MainCamera.transform.localRotation = Quaternion.identity;
-
         m_VerticalAngle = 0.0f;
         m_HorizontalAngle = transform.localEulerAngles.y;
     }
@@ -74,6 +51,39 @@ public class PlayerMovementBehaviour : MonoBehaviour
         if (!abilityPermitted)
             return;
 
+        CheckGrounded();
+        var moveVelocity = Move();
+        RotateTowardsMoveDir(moveVelocity);
+        Fall();
+    }
+
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        var destroyable = hit.gameObject.GetComponent<ExplodingInteraction>();
+        if (destroyable != null)
+        {
+            if (Speed >= destroyable.breakForce)
+                destroyable.Break();
+            return;
+        }
+
+        Rigidbody body = hit.collider.attachedRigidbody;
+        if (body == null || body.isKinematic)
+            return;
+        if (hit.moveDirection.y < -0.3)
+            return;
+
+        Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+        body.velocity = pushDir * pushPower;
+    }
+
+    void Jump(InputAction.CallbackContext context)
+    {
+        moveVelocity.y = jumpSpeed;
+    }
+
+    void CheckGrounded()
+    {
         //we define our own grounded and not use the Character controller one as the character controller can flicker
         //between grounded/not grounded on small step and the like. So we actually make the controller "not grounded" only
         //if the character controller reported not being grounded for at least .5 second;
@@ -91,80 +101,40 @@ public class PlayerMovementBehaviour : MonoBehaviour
             m_GroundedTimer = 0.0f;
             m_Grounded = true;
         }
+    }
 
-        //Vector3 move = Vector3.zero;
-        float actualSpeed = playerSpeed;
-
-        // Move around with WASD
-#if UNITY_ANDROID
-            move = new Vector3(touchMovement.x, 0, touchMovement.y);
-#elif !UNITY_WEBGL || UNITY_EDITOR
+    Vector3 Move()
+    {
         moveVelocity = new Vector3(movement.ReadValue<Vector2>().x, moveVelocity.y, movement.ReadValue<Vector2>().y);
-#else
-            if(WebGL_Lib.IsWebMobile())
-                move = new Vector3(touchMovement.x, 0, touchMovement.y);
-            else
-                move = new Vector3(movement.ReadValue<Vector2>().x, 0, movement.ReadValue<Vector2>().y);
-#endif
+
         if (moveVelocity.sqrMagnitude > 1.0f)
             moveVelocity.Normalize();
 
-        float usedSpeed = m_Grounded ? actualSpeed : m_SpeedAtJump;
+        float usedSpeed = m_Grounded ? playerSpeed : m_SpeedAtJump;
 
-        moveVelocity = moveVelocity * usedSpeed * Time.deltaTime;
+        moveVelocity *= usedSpeed * Time.deltaTime;
 
         moveVelocity = transform.TransformDirection(moveVelocity);
         m_CharacterController.Move(moveVelocity);
 
         Speed = moveVelocity.magnitude / (playerSpeed * Time.deltaTime) * playerSpeed;
+        return moveVelocity;
+    }
 
-        // Turn player
-#if UNITY_ANDROID
-            float turnPlayer = (touchRotation.x / 20) * mouseSensitivity * androidMultiplier;
-#elif !UNITY_WEBGL || UNITY_EDITOR
-        float turnPlayer = (rotation.ReadValue<Vector2>().x / 20) * mouseSensitivity * ((Gamepad.all.Count > 0) ? controllerRotationMultiplier : 1);
-#else
-            float turnPlayer = 0;
-            if(WebGL_Lib.IsWebMobile())
-                turnPlayer = (touchRotation.x / 20) * mouseSensitivity * androidMultiplier;
-            else
-                turnPlayer = (rotation.ReadValue<Vector2>().x / 20) * mouseSensitivity;
-#endif
-        m_HorizontalAngle = m_HorizontalAngle + turnPlayer;
+    void RotateTowardsMoveDir(Vector3 moveDir)
+    {
+        Debug.DrawRay(transform.position, moveDir);
 
-        if (m_HorizontalAngle > 360) m_HorizontalAngle -= 360.0f;
-        if (m_HorizontalAngle < 0) m_HorizontalAngle += 360.0f;
+        float error = Vector3.SignedAngle(transform.forward, moveDir, transform.up) / rotateAngleThreshold;
 
-        Vector3 currentAngles = transform.localEulerAngles;
-        currentAngles.y = m_HorizontalAngle;
-        transform.localEulerAngles = currentAngles;
+        error = Mathf.Clamp(error, -1f, 1f);
 
-        // Camera look up/down
-#if UNITY_ANDROID
-            var turnCam = -touchRotation.y / 20;
-            turnCam = turnCam * mouseSensitivity * androidMultiplier;
-#elif !UNITY_WEBGL || UNITY_EDITOR
-        var turnCam = -rotation.ReadValue<Vector2>().y / 20;
-        turnCam = turnCam * mouseSensitivity;
-#else
-            float turnCam = 0;
-            if (WebGL_Lib.IsWebMobile())
-            {
-                turnCam = -touchRotation.y / 20;
-                turnCam = turnCam * mouseSensitivity * androidMultiplier;
-            }
-            else
-            {
-                turnCam = -rotation.ReadValue<Vector2>().y / 20;
-                turnCam = turnCam * mouseSensitivity;
-            }
-#endif
-        m_VerticalAngle = Mathf.Clamp(turnCam + m_VerticalAngle, -89.0f, 89.0f);
-        currentAngles = CameraPosition.transform.localEulerAngles;
-        currentAngles.x = m_VerticalAngle;
-        CameraPosition.transform.localEulerAngles = currentAngles;
+        currentTargetSpeed = speed * (1 - Mathf.Abs(error));
+        currentTargetRotationSpeed = turningSpeed * error;
+    }
 
-        // Fall down / gravity
+    void Fall()
+    {
         //m_VerticalSpeed = m_VerticalSpeed - gravity * Time.deltaTime;
         //if (m_VerticalSpeed < -10.0f)
         //    m_VerticalSpeed = -10.0f; // max fall speed
@@ -179,30 +149,5 @@ public class PlayerMovementBehaviour : MonoBehaviour
         var flag = m_CharacterController.Move(moveVelocity);
         if ((flag & CollisionFlags.Below) != 0)
             moveVelocity.y = 0;
-    }
-
-    private void OnControllerColliderHit(ControllerColliderHit hit)
-    {
-        var destroyable = hit.gameObject.GetComponent<ExplodingInteraction>();
-        if (destroyable != null)
-        {
-            if (Speed >= destroyable.breakForce)
-                destroyable.Break();
-
-            return;
-        }
-
-        Rigidbody body = hit.collider.attachedRigidbody;
-        if (body == null || body.isKinematic)
-            return;
-        if (hit.moveDirection.y < -0.3)
-            return;
-
-        Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
-        body.velocity = pushDir * pushPower;
-    }
-    protected virtual void Jump(InputAction.CallbackContext context)
-    {
-        moveVelocity.y = jumpSpeed;
     }
 }
