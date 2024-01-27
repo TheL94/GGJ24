8a696205fe938e90808714af23401d0145cd8280
Francesco.Ferretti <francesco.ferretti@txtgroup.com>
2024-01-27 18:37:52 +0100

    fov ended

--- Assets/Scripts/Roomba/Behaviours/RoombaFOV.cs
+++ Assets/Scripts/Roomba/Behaviours/RoombaFOV.cs
@@ -1,31 +1,60 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class RoombaFOV : MonoBehaviour
 {
     public LayerMask mask;
     public int nRay;
-    public int degrees;
+    public float degrees;
+    public float distance;
+
+    public int leftCasted = 0;
+    public int rightCasted = 0;
 
     void FixedUpdate()
     {
-        RaycastHit hit;
-        if(nRay % 2 == 0)
-        {
+        leftCasted = 0;
+        rightCasted = 0;
 
+        RaycastHit hit;
+        float rayForDegree = 0f;
+        if(nRay % 2 != 0)
+        {
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, distance, mask))
+            {
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+                leftCasted++;
+                rightCasted++;
+            }
+            else
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * distance, Color.magenta);
+
+            rayForDegree = (float)(degrees / nRay - 1);
         }
-        // Does the ray intersect any objects excluding the player layer
-        //if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, mask))
-        //{
-        //    Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
-        //    Debug.Log("Did Hit");
-        //}
-        //else
-        //{
-        //    Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.magenta);
-        //    Debug.Log("Did not Hit");
-        //}
+        else
+            rayForDegree = (degrees / 2) / (float)nRay;
+
+        //LEFT
+        for (int i = 1; i < nRay / 2 + 1; i++)
+        {
+            if (Physics.Raycast(transform.position, Quaternion.AngleAxis(-rayForDegree * i, Vector3.up) * transform.forward, out hit, distance, mask))
+            {
+                Debug.DrawRay(transform.position, Quaternion.AngleAxis(-rayForDegree * i, Vector3.up) * transform.forward, Color.yellow);
+                leftCasted++;
+            }
+            else
+                Debug.DrawRay(transform.position, Quaternion.AngleAxis(-rayForDegree * i, Vector3.up) * transform.forward * distance, Color.magenta);
+        }
+
+        //RIGHT
+        for (int i = 1; i < nRay / 2 + 1; i++)
+        {
+            if (Physics.Raycast(transform.position, Quaternion.AngleAxis(rayForDegree * i, Vector3.up) * transform.forward, out hit, distance, mask))
+            {
+                Debug.DrawRay(transform.position, Quaternion.AngleAxis(rayForDegree * i, Vector3.up) * transform.forward, Color.yellow);
+                rightCasted++;
+            }
+            else
+                Debug.DrawRay(transform.position, Quaternion.AngleAxis(rayForDegree * i, Vector3.up) * transform.forward * distance, Color.magenta);
+        }
     }
 }
