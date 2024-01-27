385c286c9677f535cf0deddd545fe4ec54bea62d
Simone Fabbri <simonefabbri78@gmail.com>
2024-01-27 08:16:50 +0100

    Added Final score check an overwrite logic

---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class UIManager : MonoBehaviour
+{
+    private static UIManager _instance;
+
+
+    public static UIManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                Debug.LogError("UIManager NULL!");
+            }
+            return _instance;
+        }
+    }
+
+
+    public RectTransform highscoreInsertion;
+    public TextMeshProUGUI finalScore;
+    public TextMeshProUGUI currentScore;
+    public TMP_InputField Name;
+    private void Awake()
+    {
+        if (_instance)
+            Destroy(gameObject);
+        else
+            _instance = this;
+
+        DontDestroyOnLoad(this);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentScore.text = GameManager.Instance.Points+"";
+    }
+}
